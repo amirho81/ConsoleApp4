ce632d9cf79319e867c6a19ccfc26bd6d4af2b05
AsusIran <AsusIran@AMIRHAMED>
2023-11-01 14:16:03 +0330

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write(":یک عدد از 1 تا 15 وارد نمایید");
+int n = int.Parse(Console.ReadLine());
+if (n < 1 || n > 15)
+{
+    Console.WriteLine(".عدد وارد شده در بازه بالا نیست");
+}
+else
+{
+    int factorial = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        factorial = factorial * i;
+    }
+    Console.WriteLine("{1} فاکتوریل عدد {0}برابر است با ", n, factorial);
+}
+//امیر حسین جورابچی
+//5.فاکتوریل عدد n تا ۱۵ یا ۲۰
